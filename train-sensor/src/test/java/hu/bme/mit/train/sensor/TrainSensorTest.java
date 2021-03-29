527e40a9e8f6022445d7054f37308932cd3600d5
kisanami <szianami@gmail.com>
2021-03-29 20:05:32 +0200

    implementing alarm functionality, unit tests added                :

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,19 +1,46 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
+    TrainController mockTrainController;
+    TrainSensorImpl trainSensor;
+    TrainUser mockTrainUser;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        mockTrainController = mock(TrainController.class);
+        mockTrainUser = mock(TrainUser.class);
+        trainSensor = new TrainSensorImpl(mockTrainController, mockTrainUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void underAbsoluteMarginTest() {
+        trainSensor.overrideSpeedLimit(-1);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void overAbsoluteMarginTest() {
+        trainSensor.overrideSpeedLimit(550);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void betweenAbsoluteMarginTest() {
+        trainSensor.overrideSpeedLimit(300);
+        verify(mockTrainUser, times(1)).setAlarmState(false);
+    }
+
+    @Test
+    public void overRelativeMarginTest(){
+        int speed = (int) (mockTrainController.getReferenceSpeed() * 0.5 - 50);
+        trainSensor.overrideSpeedLimit(speed);
+        verify(mockTrainUser, times(1)).setAlarmState(true);
     }
 }
